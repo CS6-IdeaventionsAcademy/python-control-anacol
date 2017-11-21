22ccfac05cb1e04bbe9c144a4888a65251f8941f
anacol <anacol@ideaventionsacademy.org>
2017-11-21 11:18:49 -0500

    I an done with the favorite color one

--- c1_7_favorite_color.py
+++ c1_7_favorite_color.py
@@ -1,5 +1,17 @@
-# Name
-# Date
+# Name Alexander Nacol
+# Date 11/15/2017
 # Python Beginnings
 # 7 Favorite Color
+guesses = 0
+while "1" == "1":
+    guesses = guesses + 1
+    guess = input("What is your favorite color? ")
+    if guess == "blue":
+        break
+    else:
+        print("WHAT!? That is such a horrible color, you silly person!")
 
+print("Me too! What a coincidence!")
+guesses = str(guesses)
+print("It took you " + guesses + " guesses to guess that color.")
+    
